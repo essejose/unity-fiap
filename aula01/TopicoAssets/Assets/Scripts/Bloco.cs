1ddee98e941f9bef8a81b5f1fc6087132cf51454
logonrm <logonrm@fiap.com.br>
2017-11-27 22:41:21 -0200

    aula 01

---
+++ aula01/TopicoAssets/Assets/Scripts/Bloco.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour {
+
+
+	void OnCollisionEnter2D(Collision2D c){
+	
+		print ("colidiu");
+			
+		print (c.gameObject.tag);
+		if (c.gameObject.tag == "Player") {
+		  	
+			Destroy ( gameObject);
+			transform.Translate(0.0f, 0.0f,0.0f);
+		}
+
+	}
+
+
+	void OnCollisionStay2D( Collision2D c){
+
+
+		print ("estou colidindo");
+	}
+
+	void OnCollisionExit2D(Collision2D c){
+
+		print ("parei de colidi");
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.back * 50 * Time.deltaTime);
+	}
+}
---
+++ aula01/TopicoAssets/Assets/Scripts/Personagem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour {
+
+	public int cont;
+	public int vel;
+
+	public int velocidade = 20;
+
+	private SpriteRenderer mySpriteRender;
+
+	private Rigidbody2D myBody;
+
+	void Awake()
+	{
+		myBody = this.GetComponent<Rigidbody2D>(); 
+		mySpriteRender = this.GetComponent<SpriteRenderer> ();
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		//transform.position = new Vector2 (-5.0f, 0.0f);
+		//transform.Translate(0.0f,0.0f,0.0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate(5.0f * Time.deltaTime,0.0f,0.0f);
+		//transform.Rotate(Vector3.back * velocidade * Time.deltaTime); 
+			float move_x = Input.GetAxisRaw ("Horizontal") * velocidade * Time.deltaTime;
+			float move_y = Input.GetAxisRaw ("Vertical") * velocidade * Time.deltaTime;
+
+		transform.Translate(move_x, move_y,0.0f);
+
+
+
+		transform.Translate(move_x, 0.0f,0.0f);
+
+			
+	 
+		mySpriteRender.flipX = !(move_x >= 0);
+
+
+
+
+		  
+		if (Input.GetButtonDown ("Jump")){
+
+			myBody.AddForce(new Vector2(vel * Random.Range(10,10),200 ));
+
+		}
+			 
+
+	}
+
+	void OnMouseDown()
+	{
+
+		myBody.AddForce(new Vector2(vel * Random.Range(10,10),200 ));
+		cont++;
+		vel = vel + 5;
+	}
+}
