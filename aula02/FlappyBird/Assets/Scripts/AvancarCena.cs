9ba7bda778bc16ec5f13d507740b37b035323583
essejose <ze_pk2@hotmail.com>
2017-12-04 22:50:01 -0200

    aula02

---
+++ aula02/FlappyBird/Assets/Scripts/AvancarCena.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AvancarCena : MonoBehaviour {
+
+	public string cena;
+
+	// Update is called once per frame
+	void Update () {
+
+		//FIRE1 -> Touch, CTRL+LEFT, CLICKLEFT
+		if (Input.GetButtonDown ("Fire1")) { 
+ 
+			SceneManager.LoadScene (cena); 
+		
+		}
+
+	}
+}
---
+++ aula02/FlappyBird/Assets/Scripts/FlappyBird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyBird : MonoBehaviour {
+
+	public float impulso = 5.0f;
+	Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//
+		if (Input.GetButtonDown ("Jump")) {
+			//rb.AddForce (Vector2.up * 500.0f);
+			rb.velocity = new Vector2 (0.0f, impulso);
+
+		}
+
+
+	}
+}
