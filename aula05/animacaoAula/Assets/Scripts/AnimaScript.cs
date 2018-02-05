8440903cc6af2344df13155fc454e23cbd07c84d
essejose <ze_pk2@hotmail.com>
2018-02-05 21:40:20 -0200

    animacao

---
+++ aula05/animacaoAula/Assets/Scripts/AnimaScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimaScript : MonoBehaviour {
+
+    Animator animator;
+
+    public float velocidade;
+
+    SpriteRenderer spriteRenderer;
+
+   
+
+    // Use this for initialization
+    void Start () {
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float mover_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+
+        float mover_y = Input.GetAxisRaw("Vertical") * velocidade * Time.deltaTime;
+
+        transform.Translate(mover_x, 0.0f, 0.0f);
+        animator.SetFloat("pMover",  Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+
+        if (mover_x > 0.0f)
+        {
+            spriteRenderer.flipX = false;
+
+        }
+        else if( mover_x < 0.0f)  
+        {
+            spriteRenderer.flipX = true;
+               
+        }
+
+
+	}
+}
