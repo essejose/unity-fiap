7c9d38a0a31a7a6d1d3a5e9c89ad73bf07f6bade
essejose <ze_pk2@hotmail.com>
2018-02-05 22:46:15 -0200

    aula05

---
+++ aula05/gameMega/Assets/Scripts/BlinkScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BlinkScript : MonoBehaviour {
+
+	public float intervalo = 0.5f;
+
+	// Use this for initialization
+	IEnumerator Start () {
+	
+		GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (intervalo);
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (intervalo);
+
+		StartCoroutine (Start());
+	}
+	
+	 
+	void Update(){
+	
+
+		if(Input.GetKeyDown(KeyCode.Return)){
+			
+			SceneManager.LoadScene("games-scene");
+		}
+		
+	}
+}
