9747cabd6063d2542556e5cf6f80580e8837de8c
essejose <ze_pk2@hotmail.com>
2018-03-12 22:52:01 -0300

    aula 8

---
+++ aula08/Runner/Assets/scripts/GerarAboboras.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerarAboboras : MonoBehaviour {
+
+	public float limiteEsquerdo, limiteDireito, limiteFrontal, limiteTraseiro;
+	public float frequencia;
+	public GameObject aboboraPrefab;
+
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		yield return new WaitForSeconds (Random.Range (0.5f, frequencia));
+
+		Vector3 posicao;
+		posicao.x = Random.Range (limiteEsquerdo, limiteDireito);
+		posicao.y = 20.0f;
+		posicao.z = Random.Range (limiteFrontal, limiteTraseiro);
+		Instantiate (aboboraPrefab, posicao, transform.rotation);
+
+		StartCoroutine (Start ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ aula08/Runner/Assets/scripts/PersonagemScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemScript : MonoBehaviour {
+
+
+	Vector3 novaPosicao;
+	Animation ani ;
+
+	public float velocidade;
+	public GameObject personagem;
+
+
+	void OnCollisionEnter(Collision c){
+		print (c);
+		if (c.gameObject.tag == "item") {
+			Destroy (c.gameObject);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+		novaPosicao = transform.position;
+		ani = personagem.GetComponent<Animation> ();
+
+		ani.CrossFade ("idle");
+
+	}
+
+ 
+	// Update is called once per frame
+	void Update () {
+
+		//Touch ou click
+
+		if (Input.GetButton ("Fire1")) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+
+			if (Physics.Raycast (ray, out hit) && hit.collider.gameObject.name != "personagem-mover") {
+
+
+				novaPosicao = hit.point;
+				transform.position = Vector3.MoveTowards (transform.position,  new Vector3(novaPosicao.x,0,novaPosicao.z), velocidade * Time.deltaTime);
+				ani.CrossFade ("run"); 
+				transform.LookAt (hit.point);
+			} 
+		
+		} else {
+			ani.CrossFade ("idle");
+		}
+	
+
+
+	}
+}
